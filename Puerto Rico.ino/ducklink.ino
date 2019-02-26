c05b0ea34be2f58029a8c8d8488bd1abebd408ab
Magus Pereira <pereiram13@students.ecu.edu>
2019-02-25 22:12:02 -0500

    DDOP Beta

---
+++ Puerto Rico.ino/ducklink.ino
@@ -0,0 +1,44 @@
+#ifdef DL
+
+void setup()
+{
+  Serial.begin(115200);
+
+  iAm = "Ducklink";
+  setupDuck();
+
+  setupDisplay();
+  setupLoRa();
+  setupPortal();
+
+  Serial.println("Ducklink Online");
+  u8x8.drawString(0, 1, "Ducklink Online");
+}
+
+
+void loop()
+{
+  // Handles Captive Portal Requests
+  dnsServer.processNextRequest();
+  webServer.handleClient();
+
+  // ⚠️ Parses Civilian Requests into Data Structure
+  //offline = 
+  readData();
+
+  if (offline.phone != "")
+  {
+    sendPayload(offline);
+    showReceivedData();
+  }
+  else
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Sends Duck Stat every 30 minutes
+  sendDuckStat(offline);
+}
+
+#endif
---
+++ Puerto Rico.ino/mama.ino
@@ -0,0 +1,44 @@
+#ifdef MD
+
+void setup()
+{
+  Serial.begin(115200);
+
+  iAm = "Mama";
+  setupDuck();
+
+  setupDisplay();
+  setupLoRa();
+  setupPortal();
+
+  Serial.println("Mama Online");
+  u8x8.drawString(0, 1, "Mama Online");
+}
+
+
+void loop()
+{
+  // Handles Captive Portal Requests
+  dnsServer.processNextRequest();
+  webServer.handleClient();
+
+  // ⚠️ Parses Civilian Requests into Data Structure
+  offline = readData();
+  if (offline.fromCiv == "yes")
+  {
+    sendPayload(offline);
+    Serial.print("Sending Wifi Data");
+  }
+
+  receive(LoRa.parsePacket());
+  if (offline.fromCiv != "yes")
+  {
+    sendPayload(offline);
+    Serial.print("Sending LoRa Data");
+  }
+
+  // Sends Duck Stat every 30 minutes
+  sendDuckStat(offline);
+}
+
+#endif
